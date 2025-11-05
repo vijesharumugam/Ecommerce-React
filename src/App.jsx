b88cffcecd9efb464bdaad60c878e1d3b60e2122
vijesharumugam <vijeshvijesh2601@gmail.com>
2025-11-05 15:17:09 +0530

    ordersPage Route is connected and in components header section is created from common use

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { Routes, Route } from "react-router";
 import {HomePage} from "./pages/HomePage"; 
 import { CheckoutPage } from "./pages/Checkoutpage";
+import { OrdersPage } from "./pages/OrdersPage";
 function App() {
   
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />}>
-      </Route>
-      <Route path="Checkout" element={<CheckoutPage />}   >
-
-      </Route>
+      <Route path="/" element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
     </Routes>
     
   )
